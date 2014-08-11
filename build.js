7c153dac31a7599052aff0ded4e07ea17147ef40
Steven Lu <tacticalazn@gmail.com>
2014-08-11 11:38:08 -0400

    Making the first page.

--- build.js
+++ build.js
@@ -1,14 +1,19 @@
 var Metalsmith = require('metalsmith');
-var jade = require('metalsmith-jade');
+var templates = require('metalsmith-templates');
 var less = require('metalsmith-less');
+var beautify = require('metalsmith-beautify');
+var markdown = require('metalsmith-markdown');
 
 Metalsmith(__dirname)
-  .use(jade({
-    pretty: false
+  .use(templates({
+    engine: 'jade',
+    directory: 'templates'
   }))
   .use(less({
-
+    pattern: 'src/styles/style.less'
   }))
+  .use(markdown())
+  .use(beautify())
   .build(function(err) {
     if (err) throw err;
   });
