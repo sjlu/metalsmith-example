d6377a2f158cb77f2fd6f4a4d25bea4d753c5b58
Steven Lu <tacticalazn@gmail.com>
2014-08-11 15:38:31 -0400

    Using stylus.

--- build.js
+++ build.js
@@ -1,19 +1,15 @@
 var Metalsmith = require('metalsmith');
 var templates = require('metalsmith-templates');
 var less = require('metalsmith-less');
-var beautify = require('metalsmith-beautify');
 var markdown = require('metalsmith-markdown');
+var stylus = require('metalsmith-stylus');
 
 Metalsmith(__dirname)
   .use(templates({
     engine: 'jade',
-    directory: 'templates'
   }))
-  .use(less({
-    pattern: 'src/styles/style.less'
-  }))
+  .use(stylus())
   .use(markdown())
-  .use(beautify())
   .build(function(err) {
     if (err) throw err;
   });
